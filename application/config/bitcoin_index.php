6a11dffe2839c5be73ac7bcbf67562ed41fa2c7d
BitWasp <bitwasp@hushmail.com>
2013-10-08 19:40:04 +0100

    removing tor proxy for bitcoin price index due to some incompatibilities

--- application/config/bitcoin_index.php
+++ application/config/bitcoin_index.php
@@ -5,13 +5,4 @@ $config = array();
 
 $config['Disabled'] = array('disabled' => true);
 $config['CoinDesk'] = array('url' => 'http://api.coindesk.com/v1/bpi/currentprice.json');
-$config['CoinDesk(tor)'] = array('url' => 'http://api.coindesk.com/v1/bpi/currentprice.json',
-								 'proxy' => array(	'type' => CURLPROXY_SOCKS5,
-													'url' => 'localhost:9050')
-								);
 $config['BitcoinAverage'] = array('url' => 'https://api.bitcoinaverage.com/all');							
-
-$config['BitcoinAverage(tor)'] = array('url' => 'https://api.bitcoinaverage.com/all',
-								 'proxy' => array(	'type' => CURLPROXY_SOCKS5,
-													'url' => 'localhost:9050')
-								);
