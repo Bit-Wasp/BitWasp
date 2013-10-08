69402c1978daa8513bb68e0e66f24e1ccabd8171
BitWasp <bitwasp@hushmail.com>
2013-10-08 16:16:16 +0100

    Improved formatting of items panel, others will soon follow

--- application/views/admin/edit_items.php
+++ application/views/admin/edit_items.php
@@ -5,20 +5,17 @@
 		  <?php echo form_open('admin/edit/items', array('class' => 'form-horizontal')); ?>
 				
 		    <div class="row-fluid">
-			  <div class="span2"></div>
-			  <div class="span5"><strong>Add A Category</strong></div>
+			  <div class="span4 offset2"><strong>Add A Category</strong></div>
 		    </div>
 			  
 		    <div class="row-fluid">
-			  <div class="span1"></div>
-		      <div class="span2">Name</div>
+		      <div class="span2 offset1">Name</div>
 			  <div class="span4"><input type='text' name='category_name' value='' /></div>
 			  <span class="help-inline"><?php echo form_error('category_name'); ?></span>	
 	        </div>	
 	        
 		    <div class="row-fluid">
-			  <div class="span1"></div>				
-		      <div class="span2">Parent Category</div>
+		      <div class="span2 offset1">Parent Category</div>
 			  <div class="span4">
 			    <select name='category_parent' autocomplete="off">					
 				  <option value=''></option>
@@ -32,22 +29,19 @@
 		    </div>	
 				
 		    <div class="row-fluid">
-			  <div class="span3"></div>
-			  <div class="span4"><input type='submit' name='add_category' value='Add' class='btn btn-tiny' /></div>
+			  <div class="span4 offset3"><input type='submit' name='add_category' value='Add' class='btn btn-tiny' /></div>
 	        </div>
 		  </form>
 
 			
 		  <?php echo form_open('admin/edit/items', array('class' => 'form-horizontal')); ?>
 		    <div class="row-fluid">
-			  <div class="span2"></div>
-			  <div class="span5"><strong>Rename Category</strong></div>
+			  <div class="span4 offset2"><strong>Rename Category</strong></div>
 		    </div>
 		    
 		    <div class="row-fluid">
-			  <div class="span1"></div>
-			  <div class="span2">Category</div>
-			  <div class="span5">
+			  <div class="span2 offset1">Category</div>
+			  <div class="span4">
 				<select name='category_id' autocomplete="off">
 				  <option value=''></option>
 <?php foreach($categories as $category) { ?>
@@ -59,28 +53,24 @@
 		    </div>
 		    
 		    <div class="row-fluid">
-			  <div class="span1"></div>
-			  <div class="span2">New Name</div>
-			  <div class="span5"><input type='text' name='category_name' value='' /></div>
+			  <div class="span2 offset1">New Name</div>
+			  <div class="span4"><input type='text' name='category_name' value='' /></div>
 	          <span class="help-inline"><?php echo form_error('category_name'); ?></span>			  			  
 		    </div>
 		    
 		    <div class="row-fluid">
-			  <div class="span3"></div>
-			  <div class="span7"><input type='submit' name='rename_category' value='Rename' class='btn btn-tiny' /></div>
+			  <div class="span4 offset3"><input type='submit' name='rename_category' value='Rename' class='btn btn-tiny' /></div>
 	        </div>
 		  </form>
 			
 		  <?php echo form_open('admin/edit/items', array('class' => 'form-horizontal')); ?>
 		    
 			<div class="row-fluid">
-			  <div class="span2"></div>
-			  <div class="span5"><strong>Delete A Category</strong></div>
+			  <div class="span4 offset2"><strong>Delete A Category</strong></div>
 			</div>
 			    
 			<div class="row-fluid">
-			  <div class="span1"></div>				
-			  <div class="span2">Category</div>
+			  <div class="span2 offset1">Category</div>
 			  <div class="span4">
 				<select name='category_id' autocomplete="off">
 				  <option value=''></option>
@@ -93,8 +83,7 @@
 			</div>
 			
 		    <div class="row-fluid">
-			  <div class="span3"></div>
-			  <div class="span7"><input type='submit' name='delete_category' value='Delete' class='btn btn-tiny' /></div>
+			  <div class="span4 offset3"><input type='submit' name='delete_category' value='Delete' class='btn btn-tiny' /></div>
 	        </div>
 	        
 	      </form>
