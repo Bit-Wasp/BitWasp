928756a50bbbe78e75a73cd235a98da5c454f703
BitWasp <bitwasp@hushmail.com>
2013-10-11 13:13:29 +0100

    Adding documentation to functions in application/controllers/bitcoin.php

--- application/controllers/bitcoin.php
+++ application/controllers/bitcoin.php
@@ -21,25 +21,34 @@ class Bitcoin extends CI_Controller {
 	 * @access	public
 	 * @see		Models/Bitcoin_Model
 	 * @see		Libraries/Bw_Bitcoin
-	 * @see		Libraries/Bw_Captcha
 	 */
 	public function __construct() {
 		parent::__construct();
 		$this->load->library('bw_bitcoin');
 		$this->load->model('bitcoin_model');
 	}
-	
+
+	/**
+	 * Cash out bitcoins to an address in the database.
+	 *
+	 * @see 	Libraries/Form_Validation
+	 * @see		Libraries/Bw_Bitcoin
+	 * @see		Models/Bitcoin_Model
+	 * @return	void
+	 */	
 	public function cashout() {
 		$this->load->library('form_validation');
 		
+		// Abort if there is no cashout address set up.
 		$data['cashout_address'] = $this->bitcoin_model->get_cashout_address($this->current_user->user_hash);
 		if($data['cashout_address'] == '')
 			redirect('bitcoin');
 		
+		// Load the users current balance.
 		$data['current_balance'] = $this->bitcoin_model->current_balance();
 		
 		if($this->form_validation->run('cashout_form') == TRUE) {
-			
+			// If the form is successfully submitted.
 			$amount = $this->input->post('amount');
 				
 			$send = $this->bw_bitcoin->cashout($data['cashout_address'], (float)$amount);
@@ -53,17 +62,27 @@ class Bitcoin extends CI_Controller {
 				$update = $this->bw_bitcoin->walletnotify($send); // Add it immediately to prevent duplicates.
 				redirect('bitcoin');
 			} else {
+				// Leave an error message if the user was not redirected.
 				$data['returnMessage'] = 'There was an error processing your transaction. Please ensure you entered a positive, decimal number to send.';
 			}
 		} 
 		
+		// Reload the balance.
 		$data['current_balance'] = $this->bitcoin_model->current_balance();
 
 		$data['page'] = 'bitcoin/cashout';
 		$data['title'] = 'Cashout Funds';
 		$this->load->library('Layout', $data);	
 	}
-	
+
+	/**
+	 * Load the users Bitcoin Panel (Transactions, Topup Address, Cashout Address)
+	 *
+	 * @see 	Libraries/Form_Validation
+	 * @see		Libraries/Bw_Bitcoin
+	 * @see		Models/Bitcoin_Model
+	 * @return	void
+	 */		
 	public function panel() {
 		$this->load->library('form_validation');
 		
@@ -73,12 +92,14 @@ class Bitcoin extends CI_Controller {
 			$this->bitcoin_model->set_cashout_address($this->current_user->user_hash, $this->input->post('cashout_address'));
 		}
 		
+		// If there is any information about a recent transaction, display it.
 		$info = (array)json_decode($this->session->flashdata('info'));
 		if(count($info) !== 0){
 			$action = ($info['category'] == 'send') ? 'sent to' : 'received on';
 			$data['returnMessage'] = "{$info['value']}BTC was $action {$info['address']}.<br/ >Transaction ID: {$info['txn_id']}";
 		}
 		
+		// Load information about the user.
 		$data['current_balance'] = $this->bitcoin_model->current_balance();
 		$data['unverified_balance'] = $this->bitcoin_model->unverified_transactions($this->current_user->user_hash);
 		$data['topup_address'] = $this->bitcoin_model->get_user_address($this->current_user->user_hash);
@@ -90,15 +111,24 @@ class Bitcoin extends CI_Controller {
 		$this->load->library('Layout', $data);
 	}
 
-
-	// Callbacks
+	// Callback functions for form validation.
+	
+	/**
+	 * Check the bitcoin address is valid (may be empty to reset).
+	 *
+	 * @param	string
+	 * @return	bool
+	 */
 	public function check_bitcoin_address($param) {
-		if($param == '')
-			return TRUE;
-			
-		return $this->bw_bitcoin->validateaddress($param);
+		return ($param == '' || $this->bw_bitcoin_validateaddress($param) == TRUE) ? TRUE : FALSE;
 	}
 	
+	/**
+	 * Check the user has sufficient balance to cover the transaction.
+	 *
+	 * @param	int
+	 * @return	bool
+	 */
 	public function has_sufficient_balance($param) {
 		$balance = $this->bitcoin_model->current_balance();
 		if(($param > 0) && ((float)$param <= (float)$balance))
@@ -107,3 +137,4 @@ class Bitcoin extends CI_Controller {
 		return FALSE;
 	}
 };
+/* End of File: Bitcoin.php */
