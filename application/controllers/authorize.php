21eee16a8050186c2bac5a655f3e18cbd12f7f36
BitWasp <bitwasp@hushmail.com>
2013-10-11 13:01:40 +0100

    Adding documentation to functions in application/controllers/authorize.php

--- application/controllers/authorize.php
+++ application/controllers/authorize.php
@@ -31,46 +31,60 @@ class Authorize extends CI_Controller {
 		$this->load->model('auth_model');
 	}
 
-	// Handle password requests for sensitive pages.
+	/**
+	 * Handle password requests for sensitive pages.
+	 *
+	 * @see 	Libraries/Form_Validation
+	 * @see		Models/Users_Model
+	 * @return	void
+	 */
 	public function index(){		
-		if(is_string($this->session->userdata('current_auth_req'))){
-			$this->load->library('form_validation');
-			$this->load->model('users_model');
-			
-			$data['title'] = 'Authorize Request';
-			$data['page'] = 'authorize/password';
-			
-			if ($this->form_validation->run('authorize') == FALSE) {
-				
-				// Use the usual login form!
-				// Insert some return data to say it's an authorization request.
-				$data['returnMessage'] = 'To access this page, you must enter your password.';
-				
-			} else {
-					
-				$password = $this->input->post('password');
-				$user_info = $this->users_model->get(array('id' => $this->current_user->user_id));
-					
-				if($user_info !== FALSE){
-					$check_login = $this->users_model->check_password($this->current_user->user_name, $user_info['salt'], $password);
-					
-					if( ($check_login !== FALSE) && ($check_login['id'] == $user_info['id']) ) {
-						$uri = $this->bw_auth->successful_auth();
-						redirect($uri);
-					}
-				} 
-				
-				$data['returnMessage'] = 'Your details were incorrect! To continue with this request, you must enter your login details.';
-			}
-			$data['captcha'] = $this->bw_captcha->generate();
-			
-			$this->load->library('Layout', $data);
-		} else {
+		// Abort if there is no stored session data about an
+		// authorization request.
+		if(!is_string($this->session->userdata('current_auth_req')))
 			redirect('');
+		
+		$this->load->library('form_validation');
+		$this->load->model('users_model');
+		
+		$data['title'] = 'Authorize Request';
+		$data['page'] = 'authorize/password';
+		
+		$data['returnMessage'] = 'To access this page, you must enter your password.';
+	
+		if ($this->form_validation->run('authorize') == TRUE) {		
+			$password = $this->input->post('password');
+			$user_info = $this->users_model->get(array('id' => $this->current_user->user_id));
+
+			// Check the user info exists.
+			if($user_info !== FALSE){
+				// Check the password is valid.
+				$check_login = $this->users_model->check_password($this->current_user->user_name, $user_info['salt'], $password);
+				
+				if( ($check_login !== FALSE) && ($check_login['id'] == $user_info['id']) ) {
+					// Load the requested URI and redirect (after clearing up session data)
+					$uri = $this->bw_auth->successful_auth();
+					redirect($uri);
+				}
+			} 
+			// Leave an error message if the user was not redirected.
+			$data['returnMessage'] = 'Your details were incorrect! To continue with this request, you must enter your login details.';
 		}
+		
+		// Generate a new captcha.
+		$data['captcha'] = $this->bw_captcha->generate();
+		$this->load->library('Layout', $data);
+
 	}
 	
-	// Callback functions for form validation
+	// Callback functions for form validation.
+	
+	/**
+	 * Check the supplied answer to the Captcha is correct..
+	 *
+	 * @param	string
+	 * @return	bool
+	 */
 	public function check_captcha($param) {
 		return $this->bw_captcha->check($param);
 	}
