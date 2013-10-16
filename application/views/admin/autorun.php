1acd8bbb0119432bd21aab9ef5762b2e2a008e50
BitWasp <bitwasp@hushmail.com>
2013-10-16 20:46:42 +0100

    Removing message/transaction info from Autorun panel

--- application/views/admin/autorun.php
+++ application/views/admin/autorun.php
@@ -3,20 +3,6 @@
 		  <?php echo $nav; ?>
 			
 		  <div class="container-fluid">
-			<div class="row-fluid">
-			  <div class="span3">Transaction Count</div>
-			  <div class="span4"><?php echo $transaction_count; ?></div>
-			</div>
-			
-			<div class="row-fluid">
-			  <div class="span3">Orders Count</div>
-			  <div class="span4"><?php echo $order_count; ?></div>
-			</div>
-			
-			<div class="row-fluid">
-			  <div class="span3">Messages Count</div>
-			  <div class="span4"><?php echo $messages_count; ?></div>
-			</div>
 			
 <?php foreach($jobs as $index => $job) { 
 	if($index == 'price_index' && $config['price_index'] == 'Disabled')
