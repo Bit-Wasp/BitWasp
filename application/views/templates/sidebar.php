12d8029990ed5bcb35c72cb9c919c460f1278723
BitWasp <bitwasp@bitwasp.tk>
2013-12-16 22:15:14 +0000

    Removing multiple balance display on sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -6,9 +6,7 @@
 			  
 			<ul class="nav nav-list">
 			  <li><?php echo anchor('user/'.$current_user['user_hash'], $current_user['user_name']); ?></li>
-			  <li>Balance: BTC <?php echo $balance; ?><?php
-			  if($current_user['currency']['id'] !== '0') 
-				  echo '<br />'.$current_user['currency']['symbol'].$local_balance; ?></li>
+			  <li>Balance: <?php echo $current_user['currency']['symbol'].$local_balance; ?><br />
 			  <li><?php echo anchor('bitcoin', 'Bitcoin'); ?></li>
 <?php if($role == 'vendor') { 
 $order_str = 'My Orders'; if($count_new_orders > 0)	$order_str .= " ($count_new_orders new!)"; ?>
