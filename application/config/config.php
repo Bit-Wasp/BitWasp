b345e851ebb195a9d7e1be76c315eb690da13034
Bitwasp <bitwasp@bitwasp.tk>
2014-04-04 02:24:40 +0100

    Removed hard coded link for test.bit-wasp.org

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://test.bit-wasp.org/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
