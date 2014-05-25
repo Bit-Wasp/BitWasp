4167e9eded341ac1e368f29086c628bcc923745f
BitWasp <thomas@bitwasp.co>
2014-05-26 00:44:39 +0100

    Final tweak, needless check to remove BTC..

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -4,12 +4,12 @@
         <p class="pull-right"><i>
 <?php 
 if($price_index !== 'Disabled' && isset($exchange_rates) ) {
-		echo '1 BTC ';
+		//echo '1 BTC ';
 		foreach($exchange_rates as $code => $rate) {
-            if(! in_array($code, array('time','time_f', strtolower($currencies[0]['code']))))
-                echo '  /  '.$rate.strtoupper($code);
+            if(! in_array($code, array('time','time_f')))
+                echo ' '.$rate.strtoupper($code)." / ";
         }
-        ?> Data obtained <?php echo $exchange_rates['time_f']; ?> from <?php echo $price_index; ?>.
+        ?> Obtained <?php echo $exchange_rates['time_f']; ?> from <?php echo $price_index; ?>.
 <?php } ?></i>
 
 Delivered in {elapsed_time} seconds. <a href="https://github.com/Bit-Wasp/BitWasp">Powered by BitWasp</a></p>
